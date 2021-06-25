7e527a7e66e41c035d5d68164c05b2b1e1343d07
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-06-26 00:50:47 +0900

    Lambda とロググループ作成をセットで行うようにした

--- lambda-node/lib/lambda-node-stack.ts
+++ lambda-node/lib/lambda-node-stack.ts
@@ -1,13 +1,24 @@
 import * as cdk from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as logs from '@aws-cdk/aws-logs'
 
 export class LambdaNodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-    new lambda.Function(this, 'node-function-a', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('src/node-function-a'),
-      handler: 'index.handler'
-    })
+    createLambda(this, 'node-function-a')
   }
+}
+
+const createLambda = (scope: cdk.Construct, functionName: string) => {
+  new logs.LogGroup(scope, `${functionName}-logs`, {
+    logGroupName: `/aws/lambda/${functionName}`,
+    retention: logs.RetentionDays.ONE_WEEK,
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  })
+  return new lambda.Function(scope, functionName, {
+    runtime: lambda.Runtime.NODEJS_14_X,
+    code: lambda.Code.fromAsset(`src/${functionName}`),
+    handler: 'index.handler',
+    functionName: functionName,
+  })
 }
