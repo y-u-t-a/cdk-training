0d85b9fa3a0f70028660f00d7e344c7a288d503f
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-06-26 00:35:51 +0900

    Lambda Function を 1つ追加

--- lambda-node/lib/lambda-node-stack.ts
+++ lambda-node/lib/lambda-node-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
 
 export class LambdaNodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-
-    // The code that defines your stack goes here
+    new lambda.Function(this, 'node-function-a', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('src/node-function-a'),
+      handler: 'index.handler'
+    })
   }
 }
---
+++ lambda-node/src/node-function-a/index.js
@@ -0,0 +1,7 @@
+const handler = async (event, context) => {
+  return {
+    status: 200
+  }
+}
+
+exports.handler = handler
