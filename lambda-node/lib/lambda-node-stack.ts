9a442100e579f03ff99f9925da51353b934aea25
Yuta Ueno <y.ueno0803@gmail.com>
2023-03-11 15:32:47 +0900

    Lambda のランタイムを Node 16 にアップデート

--- lambda-node/lib/lambda-node-stack.ts
+++ lambda-node/lib/lambda-node-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core'
 import * as logs from '@aws-cdk/aws-logs'
 import * as lambdaNodeJs from '@aws-cdk/aws-lambda-nodejs'
+import { Runtime } from "@aws-cdk/aws-lambda"
 
 export class LambdaNodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -17,6 +18,7 @@ const createLambda = (scope: cdk.Construct, functionName: string, bundleModules:
     removalPolicy: cdk.RemovalPolicy.DESTROY,
   })
   return new lambdaNodeJs.NodejsFunction(scope, functionName, {
+    runtime: Runtime.NODEJS_16_X,
     entry: `src/${functionName}/index.js`,
     handler: 'handler',
     functionName: functionName,
