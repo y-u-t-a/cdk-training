bf8da31c8f8289e0e5feace7468068d7c1a309fb
Yuta Ueno <y.ueno0803@gmail.com>
2023-03-11 15:27:19 +0900

    esbuild を使う

--- lambda-node/lib/lambda-node-stack.ts
+++ lambda-node/lib/lambda-node-stack.ts
@@ -1,38 +1,28 @@
 import * as cdk from '@aws-cdk/core'
-import * as lambda from '@aws-cdk/aws-lambda'
 import * as logs from '@aws-cdk/aws-logs'
+import * as lambdaNodeJs from '@aws-cdk/aws-lambda-nodejs'
 
 export class LambdaNodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
-    createLambda(this, 'node-function-a')
-    createLambda(this, 'node-function-sdk')
+    createLambda(this, 'node-function-a', [])
+    createLambda(this, 'node-function-sdk', ["@aws-sdk/client-sts"])
   }
 }
 
-const createLambda = (scope: cdk.Construct, functionName: string) => {
+const createLambda = (scope: cdk.Construct, functionName: string, bundleModules: string[]) => {
   new logs.LogGroup(scope, `${functionName}-logs`, {
     logGroupName: `/aws/lambda/${functionName}`,
     retention: logs.RetentionDays.ONE_WEEK,
     removalPolicy: cdk.RemovalPolicy.DESTROY,
   })
-  return new lambda.Function(scope, functionName, {
-    runtime: lambda.Runtime.NODEJS_14_X,
-    code: lambda.Code.fromAsset(`src/${functionName}`, {
-      bundling: {
-        image: lambda.Runtime.NODEJS_14_X.bundlingImage,
-        command: [
-          'bash', '-c', `
-          if [ -e package-lock.json ]; then
-            npm ci
-          fi
-          cp -r . /asset-output
-          `,
-        ],
-        user: 'root' // user を root にしないと /.npm ディレクトリ作成でエラーになる
-      }
-    }),
-    handler: 'index.handler',
+  return new lambdaNodeJs.NodejsFunction(scope, functionName, {
+    entry: `src/${functionName}/index.js`,
+    handler: 'handler',
     functionName: functionName,
+    depsLockFilePath: 'package-lock.json',
+    bundling: {
+      nodeModules: bundleModules
+    }
   })
 }
