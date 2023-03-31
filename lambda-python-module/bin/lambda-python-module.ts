fc402e2f5146a2d5b2ec70069fc9203c9375485a
Yuta Ueno <y.ueno0803@gmail.com>
2023-03-31 22:38:48 +0900

    lambda-python-module を CDK v2 に移行＆ workspace化

--- lambda-python-module/bin/lambda-python-module.ts
+++ lambda-python-module/bin/lambda-python-module.ts
@@ -1,7 +1,5 @@
-#!/usr/bin/env node
-import 'source-map-support/register'
-import * as cdk from '@aws-cdk/core'
+import { App } from 'aws-cdk-lib'
 import { LambdaPythonModuleStack } from '../lib/lambda-python-module-stack'
 
-const app = new cdk.App()
+const app = new App()
 new LambdaPythonModuleStack(app, 'LambdaPythonModuleStack')
--- lambda-python-module/lib/lambda-python-module-stack.ts
+++ lambda-python-module/lib/lambda-python-module-stack.ts
@@ -1,22 +1,23 @@
-import * as cdk from '@aws-cdk/core'
-import * as lambda from '@aws-cdk/aws-lambda'
-import { LogGroup, RetentionDays } from '@aws-cdk/aws-logs'
-import { PythonFunction } from '@aws-cdk/aws-lambda-python'
+import { Construct } from 'constructs'
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs'
+import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha'
 
-export class LambdaPythonModuleStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class LambdaPythonModuleStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
     new PythonFunction(this, 'PyFunction', {
       functionName: 'PyFunction',
       entry: 'lambda',
       index: 'main.py',
       handler: 'handler',
-      runtime: lambda.Runtime.PYTHON_3_8
+      runtime: Runtime.PYTHON_3_8
     })
     new LogGroup(this, 'PyFunctionLogs', {
       logGroupName: "/aws/lambda/PyFunction",
       retention: RetentionDays.ONE_DAY,
-      removalPolicy: cdk.RemovalPolicy.DESTROY // スタック削除時に一緒に削除する
+      removalPolicy: RemovalPolicy.DESTROY // スタック削除時に一緒に削除する
     })
   }
 }
