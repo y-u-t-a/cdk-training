49d5143a46e4a7dd844fc835ecdc1ca849c473bf
Yuta Ueno <y.ueno0803@gmail.com>
2023-03-31 22:57:13 +0900

    lambda-ts を CDK v2 に移行＆workspace化

--- lambda-ts/bin/lambda-ts.ts
+++ lambda-ts/bin/lambda-ts.ts
@@ -1,6 +1,4 @@
-#!/usr/bin/env node
-import 'source-map-support/register'
-import * as cdk from '@aws-cdk/core'
+import * as cdk from 'aws-cdk-lib'
 import { LambdaTsStack } from '../lib/lambda-ts-stack'
 
 const app = new cdk.App()
--- lambda-ts/lib/lambda-ts-stack.ts
+++ lambda-ts/lib/lambda-ts-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from '@aws-cdk/core'
-import * as lambda from '@aws-cdk/aws-lambda'
-import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs'
+import { Construct } from 'constructs'
+import * as cdk from 'aws-cdk-lib'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 
 export class LambdaTsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
     new NodejsFunction(this, 'mainFunction', {
       functionName: 'TestTSFunction',
       description: 'TypeScript のテスト用関数',
       entry: 'lambda/main.ts',
       handler: 'handler',
-      runtime: lambda.Runtime.NODEJS_12_X,
+      runtime: lambda.Runtime.NODEJS_18_X,
       timeout: cdk.Duration.seconds(60),
       memorySize: 256,
       environment: {
