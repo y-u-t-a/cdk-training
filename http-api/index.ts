cb9605717e8e83cfbfa5839d2b581537636af833
Yuta Ueno <y.ueno0803@gmail.com>
2023-08-19 02:40:46 +0900

    http-api のファイル構成変更

--- http-api/index.ts
+++ http-api/index.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib'
-import { HttpApiStack } from '../lib/http-api-stack'
+import { HttpApiStack } from './lib/http-api-stack'
 
 const app = new cdk.App()
 new HttpApiStack(app, 'HttpApiStack')
