afd97b1e4d3391512292ab40b4e427a5dc90c573
Yuta Ueno <y.ueno0803@gmail.com>
2024-05-11 05:36:41 +0900

    lambda-ts の axios を import に変更

--- lambda-ts/lambda/main.ts
+++ lambda-ts/lambda/main.ts
@@ -1,11 +1,18 @@
+import axios from 'axios'
+
+type Item = {
+  id: string
+  title: string
+}
+type Response = Item[]
+
 export async function handler() {
-  const axios = require('axios')
-  const response = await (
-    await axios.get('https://my-json-server.typicode.com/typicode/demo/posts')
-  ).data
-  console.log(response)
+  const response = await axios.get<Response>('https://my-json-server.typicode.com/typicode/demo/posts')
+  console.log(`index:0 id: ${response.data?.[0].id} title: ${response.data?.[0].title}`)
+
   return {
     status: 200,
     time: `${new Date()}`,
+    data: response.data
   }
 }
