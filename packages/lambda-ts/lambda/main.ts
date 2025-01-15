6ddcd6a9b7c808d19dd995eaa6fa4ce559b66b25
Yuta Ueno <y.ueno0803@gmail.com>
2025-01-15 21:41:34 +0900

    フォーマット設定を最新化＆フォーマットを適用

--- packages/lambda-ts/lambda/main.ts
+++ packages/lambda-ts/lambda/main.ts
@@ -7,12 +7,16 @@ type Item = {
 type Response = Item[]
 
 export async function handler() {
-  const response = await axios.get<Response>('https://my-json-server.typicode.com/typicode/demo/posts')
-  console.log(`index:0 id: ${response.data?.[0].id} title: ${response.data?.[0].title}`)
+  const response = await axios.get<Response>(
+    'https://my-json-server.typicode.com/typicode/demo/posts',
+  )
+  console.log(
+    `index:0 id: ${response.data?.[0].id} title: ${response.data?.[0].title}`,
+  )
 
   return {
     status: 200,
     time: `${new Date()}`,
-    data: response.data
+    data: response.data,
   }
 }
